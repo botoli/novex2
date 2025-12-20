aa95d9e8a8efd98c8b683c3e014df1aa60a1aa2a
botoli <standrey2007@gmail.com>
2025-12-20 21:40:09 +0300

    FIX:cors 6

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,19 +3,15 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'health'],
     'allowed_methods' => ['*'],
-<<<<<<< HEAD
     'allowed_origins' => [
         'https://novextask.ru',
         'https://server-thinkpad-x220.tail44896d.ts.net',
-        'http://localhost:5173' // для локальной разработки
+        'http://localhost:3000',
+        'http://localhost:5173'
     ],
-=======
-    'allowed_origins' => ['https://novextask.ru', 'https://server-thinkpad-x220.tail44896d.ts.net', 'http://localhost:3000', 'http://localhost:5173'],
->>>>>>> 633171d975c7e79a469a652e5fb62412d216c8fe
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // ← КРИТИЧЕСКИ ВАЖНО!
+    'supports_credentials' => true,  // ← КРИТИЧЕСКИ ВАЖНО!
 ];
-
