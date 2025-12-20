1fe9b1f1bc569bc312400a148463f804a02ce936
botoli <standrey2007@gmail.com>
2025-12-20 19:03:22 +0300

    FIX:cors 2

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://novextask.ru');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-TOKEN');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
