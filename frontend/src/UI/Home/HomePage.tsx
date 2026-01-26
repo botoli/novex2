871bcb1211275326cea5246a8a632349afa9d7a6
botoli <standrey2007@gmail.com>
2026-01-26 16:40:00 +0300

    add sign up/in pages

--- frontend/src/UI/Home/HomePage.tsx
+++ frontend/src/UI/Home/HomePage.tsx
@@ -24,6 +24,8 @@ import {
 
 import AccountSettings from '../AccountSettings/AccountSettings';
 import { useData } from '../../fetch/fetchTasks';
+import PageHeader from '../../common/PageHeader';
+import Login from '../Form/Login/Login';
 
 export default function HomePage() {
   const mockAI = [
@@ -112,57 +114,8 @@ export default function HomePage() {
           <AccountSettings onclose={() => setIsOpenAccountSettings(!isOpenAccountSettings)} />
         )}
       </div>
-      <section className={styles.pageHeader}>
-        <div className={styles.searchBar}>
-          <SearchIcon />
-          <input type="text" placeholder="Search" />
-          <button>Search</button>
-        </div>
-
-        <div className={styles.avatar}>
-          <div>
-            <button>
-              <NotificationIcon />
-            </button>
-          </div>
-          {users.map(
-            (user) =>
-              user.role === 'Admin' && (
-                <div className={styles.allAccount} onClick={OpenModalProfile}>
-                  <div className={styles.account}>
-                    <div className={styles.accountInfo}>
-                      <div className={styles.accountAvatar}>
-                        <AccountIcon />
-                      </div>
-                      <div className={styles.accountDetails}>
-                        <p className={styles.accountName}>{user.name}</p>
-                        <p className={styles.accountRole}>{user.role}</p>
-                      </div>
-                    </div>
-                  </div>
-                  {isopenProfile && (
-                    <div className={styles.modalProfile}>
-                      <div
-                        className={styles.btnProfile}
-                        onClick={() => setIsOpenAccountSettings(!isOpenAccountSettings)}>
-                        <button className={styles.settings}>
-                          <SettingsIcon />
-                        </button>
-                        <p>Account settings</p>
-                      </div>
-                      <div className={styles.btnProfile}>
-                        <button className={styles.logOut}>
-                          <LogoutIcon />
-                        </button>
-                        <p>Log out</p>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              ),
-          )}
-        </div>
-      </section>
+      <Login />
+      <PageHeader />
 
       <section className={styles.dashboard}>
         <h1>Home</h1>
